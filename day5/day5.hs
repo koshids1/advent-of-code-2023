b1060aabacead84cb0d596d598424f1f13531939
koshids1 <shinji.koshida@aalto.fi>
2023-12-16 18:32:36 +0200

    Clean up day5

--- day5/day5.hs
+++ day5/day5.hs
@@ -1,5 +1,8 @@
 import Data.Char (isDigit)
 
+data Interval = Interval {initial :: Int, range :: Int}
+data MapTriple = MapTriple Int Int Int
+
 splitBy :: Eq a => a -> [a] -> [[a]]
 splitBy _ [] = []
 splitBy x xs = (fst $ breakBy x xs) : splitBy x (snd $ breakBy x xs)
@@ -16,28 +19,30 @@ mapFst f (x, y) = (f x, y)
 mapSnd :: (b -> c) -> (a, b) -> (a, c)
 mapSnd = fmap
 
-apply :: (Int, Int, Int) -> ([(Int,Int)], [(Int, Int)]) -> ([(Int,Int)], [(Int,Int)])
-apply _ ([], xps) = ([], xps)
-apply m@(t, i, r) ((j, s):rest, xps)
-    |i+r <= j                       = mapFst ((j,s):) $ apply m (rest, xps)
-    |i <= j && j < i+r && i+r < j+s = mapFst ((i+r, j+s-i-r):) $ mapSnd ((t+j-i,i+r-j):) $ apply m (rest, xps) 
-    |i <= j && j+s <= i+r           = mapSnd ((t+j-i,s):) $ apply m (rest, xps)
-    |j < i && i+r < j+s             = mapFst ((j,i-j):) $ mapFst ((i+r,j+s-i-r):) $ mapSnd ((t,r):) $ apply m (rest, xps)
-    |j < i && i < j+s && j+s <= i+r = mapFst ((j, i-j):) $ mapSnd ((t,j+s-i):) $ apply m (rest, xps)
-    |j+s <= i                       = mapFst ((j,s):) $ apply m (rest, xps)
+apply :: MapTriple -> ([Interval], [Interval]) -> ([Interval], [Interval])
+apply _ ([], xs) = ([], xs)
+apply mt@(MapTriple t i r) ((Interval j s):ys, xs)
+    |i+r <= j                       = mapFst (Interval j s:) $ apply mt (ys, xs)
+    |i <= j && j < i+r && i+r < j+s = mapFst (Interval (i+r) (j+s-i-r):) $ mapSnd (Interval (t+j-i) (i+r-j):) $ apply mt (ys, xs) 
+    |i <= j && j+s <= i+r           = mapSnd (Interval (t+j-i) s:) $ apply mt (ys, xs)
+    |j < i && i+r < j+s             = mapFst (Interval j (i-j):) $ mapFst (Interval (i+r) (j+s-i-r):) $ mapSnd (Interval t r:) $ apply mt (ys, xs)
+    |j < i && i < j+s && j+s <= i+r = mapFst (Interval j (i-j):) $ mapSnd (Interval t (j+s-i):) $ apply mt (ys, xs)
+    |j+s <= i                       = mapFst ((Interval j s):) $ apply mt (ys, xs)
+    
 
-send ::[(Int, Int, Int)] -> [(Int, Int)] -> [(Int, Int)]
-send ms xps = let (yps, zps) = go ms (xps, []) in yps ++ zps
+send :: [MapTriple] -> [Interval] -> [Interval]
+send mts xs = let (ys, zs) = go mts (xs, []) 
+               in ys ++ zs
     where
-        go :: [(Int, Int, Int)] -> ([(Int,Int)], [(Int, Int)]) -> ([(Int, Int)], [(Int, Int)])
-        go [] xpsp = xpsp 
-        go (m:ms) xpsp = go ms $ apply m xpsp
+        go :: [MapTriple] -> ([Interval], [Interval]) -> ([Interval], [Interval])
+        go [] xsp = xsp 
+        go (mt:mts) xsp = go mts $ apply mt xsp
 
-sendComp :: [[(Int, Int, Int)]] -> [(Int, Int)] -> [(Int, Int)]
-sendComp [] xps = xps 
-sendComp (ms:mss) xps = sendComp mss $ send ms xps
+sendComp :: [[MapTriple]] -> [Interval] -> [Interval]
+sendComp [] xs = xs 
+sendComp (mts:mtss) xs = sendComp mtss $ send mts xs
 
-format :: [String] -> ([Int],[[(Int, Int, Int)]])
+format :: [String] -> ([Int],[[MapTriple]])
 format xss = (seeds, maps)
     where
         yss :: [[String]]
@@ -46,28 +51,28 @@ format xss = (seeds, maps)
         seeds :: [Int]
         seeds = map read $ splitBy ' ' $ dropWhile (not . isDigit) $ head (head yss)
         
-        maps :: [[(Int, Int, Int)]]
+        maps :: [[MapTriple]]
         maps = map (map toTriple) $ map (map (map read)) $ map (map $ splitBy ' ') $ map tail $ tail yss
 
-        toTriple :: [Int] -> (Int, Int, Int)
-        toTriple xs = let k:l:m:rest = xs ++ [0,0..] in (k,l,m)
+        toTriple :: [Int] -> MapTriple
+        toTriple xs = let k:l:m:rest = xs ++ [0,0..] in MapTriple k l m
 
-ammendSeeds1 :: [Int] -> [(Int, Int)]
+ammendSeeds1 :: [Int] -> [Interval]
 ammendSeeds1 [] = []
-ammendSeeds1 (x:xs) = (x,1):ammendSeeds1 xs
+ammendSeeds1 (x:xs) = (Interval x 1):ammendSeeds1 xs
 
-ammendSeeds2 :: [Int] -> [(Int,Int)]
+ammendSeeds2 :: [Int] -> [Interval]
 ammendSeeds2 [] = []
 ammendSeeds2 (x:[]) = []
-ammendSeeds2 (x:y:rest) = (x,y):ammendSeeds2 rest
+ammendSeeds2 (x:y:xs) = (Interval x y):ammendSeeds2 xs
 
-getLocation :: ([(Int, Int)], [[(Int, Int, Int)]]) -> [(Int, Int)]
+getLocation :: ([Interval], [[MapTriple]]) -> [Interval]
 getLocation x = (sendComp $ snd x) $ fst x
 
 main :: IO ()
 main = do
     contents <- readFile "input"
     putStr "Part 1: "
-    print $ minimum $ map fst $ getLocation $ mapFst ammendSeeds1 $ format $ lines contents
+    print $ minimum $ map initial $ getLocation $ mapFst ammendSeeds1 $ format $ lines contents
     putStr "Part 2: "
-    print $ minimum $ map fst $ getLocation $ mapFst ammendSeeds2 $ format $ lines contents
+    print $ minimum $ map initial $ getLocation $ mapFst ammendSeeds2 $ format $ lines contents
